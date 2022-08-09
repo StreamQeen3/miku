cf327f729ca3db31e3ecf0e54779540508c7ed36
Devil7777777 <110847305+Devil7777777@users.noreply.github.com>
2022-08-09 18:34:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
+global.Owner = ['916296987272']
+global.OwnerNumber = ['916296987272']
+global.ownertag = ['916296987272']
 global.BotName = "Miku"
 global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.author = "By: Dipanjan"
+global.OwnerName = "Dipanjan"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
